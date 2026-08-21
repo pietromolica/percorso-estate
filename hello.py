bb6c312d55ba4e4074d6fe6d5d5cc136dc0bf6c4
pietromolica <molicabaratttap@gmail.com>
2026-08-21 10:26:21 +0200

    Versione compresa anche di claasi e funzioni

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
 print("Ciao, percorso estate")
 print("oggi ho imparato git e github")
+
+
+
+class persona:
+    def saluta(self):
+        nome = input("Come ti chiami?:")
+        print(f"Ciao {nome} :)")
+        
+
+p = persona()
+p.saluta()
+    
